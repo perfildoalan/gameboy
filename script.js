552a2bc167cd023094dc68cf49d05ad237e29592
alan <enviarparaalan@gmail.com>
2023-06-15 00:17:04 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,234 @@
+// const valorTextoInicial = document.createTextNode("Bem-vindo");
+// const elementoDoTextoInicial = document.createElement("p");
+// // elementoDoTextoInicial.style.zIndex = 2;
+// elementoDoTextoInicial.appendChild(valorTextoInicial);
+// elementoDoTextoInicial.id = 'textoInicial';
+// elementoDoTextoInicial.className = "textoInicial";
+
+// var textoInicialNaTela = document.getElementById('tela');
+// textoInicialNaTela.appendChild(elementoDoTextoInicial);
+
+
+
+//-----------------------------------------------------------------------------
+
+const video = document.createElement('video');
+video.id = 'video-gameboy';
+video.src = 'assets/gameboy.mp4';
+video.width = 800;
+video.height = 720;
+
+const circuloBotaoStart = document.getElementById("circulo-botao-start")
+circuloBotaoStart.addEventListener("click", start)
+window.addEventListener("keydown", enterJogo, false);
+
+// var elementoBlackPaper = document.getElementById("black-paper")
+// elementoBlackPaper.addEventListener("click", parabensVolta, false);
+
+function enterJogo (evento) {
+  var valorEnter = evento.keyCode
+  if (valorEnter == 13) {
+    start ();
+    window.removeEventListener("keydown", enterJogo);
+  }
+}
+
+// ----------------------------------------------------------------------------
+
+function parabens (time) {
+  function parabensEtapa1 () {
+    var elementoParabens = document.getElementById("black-paper")
+    elementoParabens.classList.remove("black-paper-off")
+    var textoTempo = document.getElementById("tempo-venceu")
+    textoTempo.innerText = textoTempo.innerText + parseInt(time/1000) + " segundos";
+  }
+  setTimeout(parabensEtapa1, 300);
+}
+
+function parabensVolta () {
+  function parabensVoltaEtapa1 () {
+    var elementoParabens = document.getElementById("black-paper")
+    elementoParabens.classList.add("black-paper-off")
+  }
+  setTimeout(parabensVoltaEtapa1, 300);
+}
+
+// ----------------------------------------------------------------------------
+
+function botoesSelecionados (botao) {
+  var botaoPosicao = document.getElementById(botao)
+  botaoPosicao.classList.add("botao-posicao-selecionado")
+
+  function voltarCorPosicao(botao) {
+    var botaoPosicaoSelecionado = document.getElementById(botao)
+    botaoPosicaoSelecionado.classList.remove("botao-posicao-selecionado")
+  }
+  setTimeout(()=>{voltarCorPosicao(botao)}, 200);
+}
+
+// ---------------------------------------------------------------------------
+
+function start (){
+
+  const start = Date.now();
+
+  var botaoStart = document.getElementById("botao-start")
+  botaoStart.classList.add("botao-start-select-selecionado")
+
+  function voltarCorBotaoStartSelect() {
+    botaoStart.classList.remove("botao-start-select-selecionado")
+  }
+
+  setTimeout(voltarCorBotaoStartSelect, 200);
+
+  const tela = document.getElementById("tela");
+  tela.appendChild(video);
+
+  setTimeout(saidaVideo, 5000);
+  function saidaVideo() {
+    const apagarVideo = document.getElementById("tela");
+    tela.removeChild(video);
+  }
+
+  function playVideo() {
+    video.play();
+  }
+
+  setTimeout(playVideo, 500);
+
+  function labirinto(){
+    var cnv = document.querySelector("canvas")
+    var ctx = cnv.getContext("2d")
+
+    var left = 37, up = 38, right = 39, down = 40;
+
+    ctx.scale(1, 0.595)
+    var inicialText = "Aperte Enter para começar"
+
+    var tileSize = 12;
+    var player = {
+      columnPlayer: 0,
+      rowPlayer: 0,
+      width: 12,
+      height: 12,
+    }
+
+    var posicaoGanhador = {
+      columnGanhador: 0,
+      rowGanhador: 0,
+    }
+    var maze = [
+      [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+      [1,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1],
+      [1,0,1,1,1,1,0,1,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,0,1],
+      [1,0,1,0,0,1,0,1,0,0,0,0,0,0,0,1,0,1,0,0,0,0,1,0,1],
+      [1,0,1,0,0,1,0,1,0,1,1,1,1,1,1,1,0,1,1,1,1,0,1,0,1],
+      [1,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1],
+      [1,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,0,1],
+      [1,0,1,0,0,1,0,1,1,1,1,1,1,1,1,0,1,0,0,0,0,0,0,0,1],
+      [1,0,1,0,0,1,0,0,0,0,0,0,0,0,1,0,1,0,1,1,1,1,1,0,1],
+      [1,0,1,0,0,1,1,1,1,1,1,1,1,0,1,0,1,0,1,0,0,0,1,0,1],
+      [1,0,1,0,0,0,0,0,0,0,0,0,1,0,1,0,1,0,1,1,1,0,1,0,1],
+      [1,0,1,1,1,1,1,1,1,1,0,0,1,0,1,0,1,0,0,0,0,0,1,0,1],
+      [1,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,1,1,1,1,1,1,1,0,1],
+      [1,0,0,0,0,0,0,0,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,0,1],
+      [1,0,1,1,1,1,1,1,1,1,0,0,1,0,1,1,1,1,1,1,1,1,1,0,1],
+      [1,0,1,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,1,0,1],
+      [1,0,1,0,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1],
+      [1,0,1,1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,1,0,1],
+      [1,0,0,0,0,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+      [1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,3,1],
+      [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    ];
+
+    window.addEventListener("keydown", keydownHandler, false);
+
+    function keydownHandler (evento) {
+      var valorTecla = evento.keyCode
+      if (valorTecla === left && maze[player.rowPlayer][player.columnPlayer-1] !== 1) {
+        maze[player.rowPlayer][player.columnPlayer] = 0
+        maze[player.rowPlayer][player.columnPlayer-1] = 2
+        botoesSelecionados("botao-left");
+      }
+      else if (valorTecla === up && maze[player.rowPlayer-1][player.columnPlayer] !== 1) {
+        maze[player.rowPlayer][player.columnPlayer] = 0
+        maze[player.rowPlayer-1][player.columnPlayer] = 2
+        botoesSelecionados("botao-top");
+      }
+      else if (valorTecla === right && maze[player.rowPlayer][player.columnPlayer+1] !== 1) {
+        maze[player.rowPlayer][player.columnPlayer] = 0
+        maze[player.rowPlayer][player.columnPlayer+1] = 2
+        botoesSelecionados("botao-right");
+      }
+      else if (valorTecla === down && maze[player.rowPlayer+1][player.columnPlayer] !== 1) {
+        maze[player.rowPlayer][player.columnPlayer] = 0
+        maze[player.rowPlayer+1][player.columnPlayer] = 2
+        botoesSelecionados("botao-down");
+      }
+      ganhador();
+    }
+
+    function ganhador () {
+      if (player.rowPlayer === posicaoGanhador.rowGanhador && player.columnPlayer === posicaoGanhador.columnGanhador) {
+        window.removeEventListener("keydown", keydownHandler);
+        const end = Date.now();
+        parabens(end-start);
+      }
+    }
+
+    function update(){}
+
+    function render(){
+      ctx.save();
+      for(var row in maze) {
+        for(var column in maze[row]) {
+          var tile = maze[row][column];
+          if(tile === 1){
+            var x = column*tileSize;
+            var y = row*tileSize;
+            ctx.fillRect(x,y,tileSize,tileSize);
+            ctx.restore();
+            ctx.save();
+          }
+          else if(tile === 2) {
+            ctx.fillStyle = "#048795";
+            var x = column*tileSize
+            var y = row*tileSize
+            player.columnPlayer = parseInt(column)
+            player.rowPlayer = parseInt(row)
+            ctx.fillRect(x,y,player.width,player.height)
+            ctx.restore();
+            ctx.save();
+          }
+          else if(tile === 3) {
+            ctx.fillStyle = "#ea4a3f";
+            var x = column*tileSize
+            var y = row*tileSize
+            posicaoGanhador.columnGanhador = parseInt(column)
+            posicaoGanhador.rowGanhador = parseInt(row)
+            ctx.fillRect(x,y,player.width,player.height)
+            ctx.restore();
+            ctx.save();
+          }
+          else {
+            ctx.fillStyle = "#adff5a";
+            var x = column*tileSize
+            var y = row*tileSize
+            ctx.fillRect(x,y,player.width,player.height)
+            ctx.restore();
+            ctx.save();
+          }
+        }
+      }
+    }
+
+    function loop(){
+      update();
+      render();
+      requestAnimationFrame(loop,cnv);
+    }
+    requestAnimationFrame(loop,cnv);
+  };
+
+  setTimeout(labirinto, 5000);
+}
